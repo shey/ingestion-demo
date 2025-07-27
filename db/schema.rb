6c6a0348c251e09efcbd12865be203b50d02e143
shey <shey@shey.ca>
2025-07-27 19:22:50 -0400

    api key

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_27_192007) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_27_232242) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -19,5 +19,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_27_192007) do
     t.datetime "received_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "api_key"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["api_key"], name: "index_users_on_api_key"
   end
 end
